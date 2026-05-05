9c1771b7e6feddd990d8c7200f0ec31e28bdc4c4
jiwon <mimi1408@naver.com>
2026-05-05 22:44:06 +0900

    5/5

--- js/company.js
+++ js/company.js
@@ -7,10 +7,10 @@ $(function () {
     direction: "vertical",
     slidesPerView: 1,
     mousewheel: {
-      releaseOnEdges: true, // ⭐ 핵심: 슬라이드가 처음/끝일 때만 페이지 스크롤 허용
-      sensitivity: 1,       // 휠 감도 (너무 빠르면 늘리세요)
+      releaseOnEdges: true, 
+      sensitivity: 1,      
     },
-    speed: 800,             // 전환 속도를 조금 늦춰야 사용자가 멈춘 느낌을 받습니다.
+    speed: 800,
     observer: true,
     observeParents: true,
   });
